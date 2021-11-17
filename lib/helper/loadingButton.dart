d0aafe6ea1159cc6c5141a3dce4c21fe0c009c93
unknown <herisetyawan233@gmail.com>
2021-11-17 10:28:59 +0700

    fix conflict

--- lib/helper/loadingButton.dart
+++ lib/helper/loadingButton.dart
@@ -1,1 +1,41 @@
 import 'package:flutter/material.dart';
+
+import 'config.dart';
+
+class LoadingButton extends StatelessWidget {
+  const LoadingButton({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        fixedSize: Size(MediaQuery.of(context).size.width, 50),
+        primary: Config.primary,
+        onPrimary: Config.textWhite,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 20,
+            height: 20,
+            child: CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Text(
+            "Tunggu...",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ],
+      ),
+    );
+  }
+}
